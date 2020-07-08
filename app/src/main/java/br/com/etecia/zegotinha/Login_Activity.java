d73c637340bb67587484d3109504a112a0f969db
andrebos17 <andre.os908@gmail.com>
2020-07-08 18:10:35 -0300

    Loggin
    Signed-off-by: andrebos17 <andre.os908@gmail.com>

---
+++ app/src/main/java/br/com/etecia/zegotinha/Login_Activity.java
@@ -0,0 +1,14 @@
+package br.com.etecia.zegotinha;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Login_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_layout);
+    }
+}
